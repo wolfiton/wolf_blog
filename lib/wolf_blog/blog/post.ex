736a5e080d8d297cc0c6caf388869af82466c7d6
Draguta Dan <dragutadan@gmail.com>
2020-02-19 14:33:03 +0200

    Generated the Blog Context with the Post Schema

---
+++ lib/wolf_blog/blog/post.ex
@@ -0,0 +1,19 @@
+defmodule WolfBlog.Blog.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "posts" do
+    field :body, :string
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:title, :body])
+    |> validate_required([:title, :body])
+    |> unique_constraint(:title)
+  end
+end
