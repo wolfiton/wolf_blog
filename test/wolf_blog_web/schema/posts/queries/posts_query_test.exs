812d279a66ff631c2b8e5d8b4bc6537a3bcc043b
Draguta Dan <dragutadan@gmail.com>
2020-03-05 22:06:28 +0200

    Final version

--- test/wolf_blog_web/schema/posts/queries/posts_query_test.exs
+++ test/wolf_blog_web/schema/posts/queries/posts_query_test.exs
@@ -18,11 +18,6 @@ defmodule WolfBlogWeb.Schema.Posts.Queries.PostsQueryTest do
   describe "Testing the Posts Query" do
     test "Should get all posts(1 of 1)" do
 
-      # absinthe_title =  "Absinthe is great"
-      # absinthe_body =  "Absinthe can really make working with Phoenix and Graphql much easier.The big advantage is that you can also test the code."
-
-      # %Post{title: absinthe_title , body: absinthe_body} |> Repo.insert()
-
       conn = build_conn()
 
       res = get conn, "/graphql", query: @query
