037ca20034359e4b6786625dbc8a228fb7ce5da2
Draguta Dan <dragutadan@gmail.com>
2020-03-05 20:58:29 +0200

    Modified the version that works

--- test/wolf_blog_web/schema/posts/queries/posts_query_test.exs
+++ test/wolf_blog_web/schema/posts/queries/posts_query_test.exs
@@ -1,10 +1,10 @@
 defmodule WolfBlogWeb.Schema.Posts.Queries.PostsQueryTest do
   use WolfBlogWeb.ConnCase, async: true
 
-  alias WolfBlog.AbsintheHelpers
 
-  alias WolfBlog.Blog.Post
-  alias WolfBlog.Repo
+  setup do
+    WolfBlog.Seeder.power_up()
+  end
 
 
   @query """
@@ -16,15 +16,17 @@ defmodule WolfBlogWeb.Schema.Posts.Queries.PostsQueryTest do
   """
 
   describe "Testing the Posts Query" do
-    test "Should get all posts(1 of 1)", context do
+    test "Should get all posts(1 of 1)" do
 
-      absinthe_title =  "Absinthe is great"
-      absinthe_body =  "Absinthe can really make working with Phoenix and Graphql much easier.The big advantage is that you can also test the code."
+      # absinthe_title =  "Absinthe is great"
+      # absinthe_body =  "Absinthe can really make working with Phoenix and Graphql much easier.The big advantage is that you can also test the code."
 
-      %Post{title: absinthe_title , body: absinthe_body} |> Repo.insert() 
+      # %Post{title: absinthe_title , body: absinthe_body} |> Repo.insert()
 
-      res = context.conn
-        |> post("/graphql", AbsintheHelpers.query_skeleton(@query, "posts"))
+      conn = build_conn()
+
+      res = get conn, "/graphql", query: @query
+
 
       assert json_response(res, 200)["data"]["posts"] == [%{"title" => "Absinthe is great"}]
     end
